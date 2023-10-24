5482c657a4b8166bb8abb3df93517af74e2bf622
Sohyang Bak <sohko77@gmail.com>
2023-10-25 00:35:31 +0900

    Update 상어 중학교.py
    하~완벽히 이해했다~ 나는 바보^^ 변수 선언을 안했었네...그래서 런타임 네임에러가 났었구나...내가 쓰는 파이썬 컴파일러는 3.11이라서 if문 처리과정에서 백준의 파이썬 3.7 컴파일러와 차이가 있었다... 그래서 내환경에서는 잘되고 백준에서는 오류가 떴었던...아무튼 변수선언은 기본인데...이걸 발견못해서 2시간이나 헤매다니..너무 슬프지만 잘해결되어서 기쁘다..

--- 백준/Gold/21609. 상어 중학교/상어 중학교.py
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.py
@@ -1,8 +1,13 @@
 from collections import deque
-
+""" Input """
 n, m = map(int,input().split())
 boards = [list(map(int,input().split())) for _ in range(n)]
+
 score = 0
+
+numBlocks = 0
+numRainbowBlocks = 0
+blockCoords = []
 
 def isOut(x,y):
     if x < 0 or x >= n: return True
@@ -41,13 +46,13 @@ def findGroup(x,y,color):
     for i,j in rainbowCoords:
         visited[i][j] = 0
         
-    return [numBlocks, numRainbowBlocks, blockCoords]
+    return numBlocks, numRainbowBlocks, blockCoords
     
 def remove(blockCoords):
     for i, j in blockCoords:
         boards[i][j] = -2 # 비워져있는 건 -2
 
-def gravitate(boards):
+def gravitate():
     for j in range(n):
         for i in reversed(range(n-1)):
             if (0 <= boards[i][j] <= m) and boards[i+1][j] == -2:
@@ -56,46 +61,67 @@ def gravitate(boards):
                     boards[x][j] = boards[x-1][j]
                     boards[x-1][j] = -2
                     x = x+1
+    return 0
+    """
+def gravitate(boards):
+    for i in range(n-2, -1, -1):
+        for j in range(n):
+            if boards[i][j] > -1: 
+                r = i
+                while True:
+                    if 0<=r+1<n and boards[r+1][j] == -2:  # 다음행이 인덱스 범위 안이면서 -2이면 아래로 다운
+                        boards[r+1][j] = boards[r][j]
+                        boards[r][j] = -2
+                        r += 1
+                    else:
+                        break
+                            """
 
 
 def rotate(boards):
     tmpB = [[0 for _ in range(n)] for _ in range(n)]
-    for j in reversed(range(n)):
-        for i in range(n):
+    for i in range(n):
+        for j in range(n):
             if not isOut(n-j-1,i):
                 tmpB[n-j-1][i] = boards[i][j]
-    boards = tmpB
+    # boards = tmpB
     return tmpB
 
 while True:
     visited = [[0 for _ in range(n)] for _ in range(n)]
-    blocks = []
     # 블록 그룹들 모두 탐색
     for i in range(n):
         for j in range(n):
-            if boards[i][j] > 0 and not visited[i][j]:  # 일반블록이면서 방문 안했으면
-                visited[i][j] = 1  # 방문
-                block_info = findGroup(i, j, boards[i][j])  # 인접한 블록 찾기
-                # block_info = [블록크기, 무지개블록 개수, 블록좌표]
-                if block_info[0] >= 2:
-                    blocks.append(block_info)
-    blocks.sort(reverse=True)
+            if visited[i][j] == 0 and (1 <= boards[i][j] <= m):
+                visited[i][j] = 1
+                nB, nRB, bC = findGroup(i,j,boards[i][j])
+                # 크기가 가장 큰 블록 그룹 찾기
+                if (i==0 and j==0) or (nB > numBlocks) or (nB == numBlocks and nRB >= numRainbowBlocks):
+                    numBlocks = nB
+                    numRainbowBlocks = nRB
+                    blockCoords = bC
 
     # 블록 그룹이 하나도 없다면 종료
-    if not blocks:
+    if numBlocks <= 1:
         break
-
-    remove(blocks[0][2])
-    score += blocks[0][0]**2
+        
+    # 그룹이 존재한다면, 찾은 블록 그룹 제거 -> 중력적용 -> 90회전 -> 중력적용
+    #print("remove 수행 전 boards : ", boards)
+    remove(blockCoords)
+    #print("remove 수행 후 boards : ", boards)
+    score += numBlocks ** 2
+    #print(score)
     
-    # 4. 중력
-    gravitate(boards)
-
-    # 5. 90회전
+    gravitate()
+    #print("gravitate1 수행 후 boards : ", boards) 
     boards = rotate(boards)
-
-    # 6. 중력
-    gravitate(boards)
+    #print("rotate 수행 후 boards : ", boards)
+    gravitate()
+    #print("gravitate2 수행 후 boards : ", boards)
+    #print("visited 출력 : ", visited)
+    numBlocks = 0
+    numRainbowBlocks = 0
+    blockCoords = []
 
 print(score)
             
