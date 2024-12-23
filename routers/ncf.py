6829239498a647fdaedb35febabeb6e7515c499a
Sohyang Bak <sohko77@gmail.com>
2024-12-23 14:50:38 +0900

    Update ncf.py
    pullX-test4

--- routers/ncf.py
+++ routers/ncf.py
@@ -9,3 +9,5 @@ async def start_ncf():
 @ncf.get('/model', tags = ['ncf'])
 async def ncf_model():
     return {'msg' : 'Here is NCF model'} 
+
+print("원격저장소에서 직접 수정을 두 번째로 했어요! 이건 pull을 안했을 때 어떤 충돌과 오류가 나는지 확인하기 위함입니다-!!!")
